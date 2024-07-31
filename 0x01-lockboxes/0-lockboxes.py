597357c7abe20612da81cf35d2444a16ba425313
Madani00 <mamchart@gmail.com>
2024-07-31 15:20:21 +0000

    good

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Determines if all the boxes can be opened"""
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    visited = set()
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        if box not in visited:
+            visited.add(box)
+            for key in boxes[box]:
+                if key not in visited:
+                    stack.append(key)
+
+    return len(visited) == n
