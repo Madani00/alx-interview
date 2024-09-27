3a6b24641ee8c7950677645d7d722928b7f5fdbd
Madani00 <mamchart@gmail.com>
2024-09-27 12:53:13 -0700

    well

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,7 +15,7 @@ def island_perimeter(grid):
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
                 if j == cols - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1    
+                    perimeter += 1
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
                 if i == rows - 1 or grid[i + 1][j] == 0:
